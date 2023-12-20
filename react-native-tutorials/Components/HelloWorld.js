78e5356f5d83586513c3964a2249a6498ac68329
SaeedAchiever <saeedleverage101@gmail.com>
2023-12-20 03:37:22 +0000

    A Hello World Component

---
+++ react-native-tutorials/Components/HelloWorld.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <View>
+
+      {/* A Hello World Component */}
+
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default HelloWorld
