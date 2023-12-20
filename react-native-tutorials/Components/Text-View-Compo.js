88278fa7a2f4166dbe2e52ab373ea6ae69c7e5cb
SaeedAchiever <saeedleverage101@gmail.com>
2023-12-20 04:15:16 +0000

    Text and View Component

---
+++ react-native-tutorials/Components/Text-View-Compo.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React, { Component } from 'react'
+
+const TextAndView = () => {
+  return (
+
+      // A Text  Component is  used  to  render  text  to  the UI
+
+      // View  on  the other hand  serves as  container for 
+      // sub-components  and sections of  the main  application
+
+    <View>
+      <Text>Text  And View</Text>
+    </View>
+  )
+}
+
+export default TextAndView
